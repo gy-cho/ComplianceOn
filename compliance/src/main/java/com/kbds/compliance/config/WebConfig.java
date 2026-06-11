a6c41de932da45afff468c32fc5b7d10237b1832
cgr <cgr070112@gmail.com>
2026-06-11 09:43:34 +0900

    서버 프록시 설정

--- compliance/src/main/java/com/kbds/compliance/config/WebConfig.java
+++ compliance/src/main/java/com/kbds/compliance/config/WebConfig.java
@@ -1,15 +1,30 @@
 package com.kbds.compliance.config;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 2. 스프링이 이 설정을 읽도록 어노테이션 필수!
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:/home/kbds/Documents/images/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")   // ← 여기
+            // .allowedOrigins(allowedOrigins.toArray(new String[0]))
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*");
     }
 }
