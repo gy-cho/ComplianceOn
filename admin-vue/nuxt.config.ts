17f47d4586e78abc556705b7341d95cbff73cad8
cgr <cgr070112@gmail.com>
2026-06-19 14:23:23 +0900

    admin 빌드시 아이콘 파일 포함

--- admin-vue/nuxt.config.ts
+++ admin-vue/nuxt.config.ts
@@ -11,6 +11,12 @@ export default defineNuxtConfig({
     },
   },
   modules: ['@nuxt/icon'],
+  icon: {
+    // 내부망(오프라인) 환경 대응: iconify API로 fallback하지 않고
+    // 빌드 시점에 @iconify-json/lucide에서 아이콘을 서버 번들로 포함시킴
+    provider: 'server',
+    fallbackToApi: false,
+  },
   css: ['~/assets/css/global.css'],
   devServer: {
     host: '0.0.0.0',
